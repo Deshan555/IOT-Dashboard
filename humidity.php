8db67346b8a4349eafd4123d0353b1d0567a220e
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-04-12 09:06:52 +0530

    remove unwanted js functions from humidity.php file

--- humidity.php
+++ humidity.php
@@ -55,79 +55,79 @@ $sensor_data = $stmt->get_result();
             </li>
         </ul>
     </nav>
-        <?php include'sidebar.php' ?>
+    <?php include 'sidebar.php' ?>
 
-        <div id="layoutSidenav_content">
-            <main>
-                <div class="container-fluid px-4">
-                    <h1 class="mt-4">Charts</h1>
-                    <ol class="breadcrumb mb-4">
-                        <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                        <li class="breadcrumb-item active">Humidity</li>
-                    </ol>
-                    <div class="card mb-4">
-                        <div class="card-body">
-                            Chart.js is a third party plugin that is used to generate the charts in this template. The charts below have been customized - for further customization options, please visit the official
-                            <a target="_blank" href="https://www.chartjs.org/docs/latest/">Chart.js documentation</a>
-                            .
+    <div id="layoutSidenav_content">
+        <main>
+            <div class="container-fluid px-4">
+                <h1 class="mt-4">HUMIDITY</h1>
+                <ol class="breadcrumb mb-4">
+                    <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                    <li class="breadcrumb-item active">Humidity</li>
+                </ol>
+                <div class="card mb-4">
+                    <div class="card-body">
+                        Chart.js is a third party plugin that is used to generate the charts in this template. The charts below have been customized - for further customization options, please visit the official
+                        <a target="_blank" href="https://www.chartjs.org/docs/latest/">Chart.js documentation</a>
+                        .
+                    </div>
+                </div>
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <i class="fas fa-chart-area me-1"></i>
+                        Area Chart Example
+                    </div>
+                    <!--<div class="card-body"><canvas id="myAreaChart" width="100%" height="30"></canvas></div>-->
+                    <!--<div class="card-body"><canvas id="line_graps" style="width:100%;max-width:600px"></canvas></div>-->
+                    <div class="card-body"><canvas id="areaChart" width="100%" height="30"></canvas></div>
+                    <div class="card-footer small text-muted">Last Updated <?php echo date("Y.m.d") ?></div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-chart-bar me-1"></i>
+                                Bar Chart Example
+                            </div>
+                            <!--<div class="card-body"><canvas id="myBarChart" width="100%" height="50"></canvas></div>-->
+                            <!--<div id="piechart" style="width:100%; max-width:600px; height:500px;"></div>-->
+                            <div class="card-body"><canvas id="myChart2" style="width:100%; max-width:600px"></canvas></div>
+                            <div class="card-footer small text-muted">Last Updated <?php echo date("Y.m.d") ?></div>
                         </div>
                     </div>
-                    <div class="card mb-4">
-                        <div class="card-header">
-                            <i class="fas fa-chart-area me-1"></i>
-                            Area Chart Example
-                        </div>
-                        <!--<div class="card-body"><canvas id="myAreaChart" width="100%" height="30"></canvas></div>-->
-                        <!--<div class="card-body"><canvas id="line_graps" style="width:100%;max-width:600px"></canvas></div>-->
-                        <div class="card-body"><canvas id="areaChart" width="100%" height="30"></canvas></div>
-                        <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="card mb-4">
-                                <div class="card-header">
-                                    <i class="fas fa-chart-bar me-1"></i>
-                                    Bar Chart Example
-                                </div>
-                                <!--<div class="card-body"><canvas id="myBarChart" width="100%" height="50"></canvas></div>-->
-                                <!--<div id="piechart" style="width:100%; max-width:600px; height:500px;"></div>-->
-                                <div class="card-body"><canvas id="myChart2" style="width:100%; max-width:600px"></canvas></div>
-                                <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+                    <div class="col-lg-6">
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-chart-pie me-1"></i>
+                                Pie Chart Example
                             </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="card mb-4">
-                                <div class="card-header">
-                                    <i class="fas fa-chart-pie me-1"></i>
-                                    Pie Chart Example
-                                </div>
-                                <!--<div class="card-body"><canvas id="myPieChart" width="100%" height="50"></canvas></div>-->
-                                <!--<div id="myChart" style="width:100%; max-width: 900px; height:500px;"></div>-->
-                                <div class="card-body"><canvas id="myChart" style="width:100%;max-width:600px"></canvas></div>
-                                <div class="card-footer small text-muted">Updated <?php echo date("Y.m.d") ?></div>
-                            </div>
+                            <!--<div class="card-body"><canvas id="myPieChart" width="100%" height="50"></canvas></div>-->
+                            <!--<div id="myChart" style="width:100%; max-width: 900px; height:500px;"></div>-->
+                            <div class="card-body"><canvas id="myChart" style="width:100%;max-width:600px"></canvas></div>
+                            <div class="card-footer small text-muted">Last Updated <?php echo date("Y.m.d") ?></div>
                         </div>
                     </div>
                 </div>
-            </main>
-            <footer class="py-4 bg-light mt-auto">
-                <div class="container-fluid px-4">
-                    <div class="d-flex align-items-center justify-content-between small">
-                        <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                        <div>
-                            <a href="#">Privacy Policy</a>
-                            &middot;
-                            <a href="#">Terms &amp; Conditions</a>
-                        </div>
+            </div>
+        </main>
+        <footer class="py-4 bg-light mt-auto">
+            <div class="container-fluid px-4">
+                <div class="d-flex align-items-center justify-content-between small">
+                    <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                    <div>
+                        <a href="#">Privacy Policy</a>
+                        &middot;
+                        <a href="#">Terms &amp; Conditions</a>
                     </div>
                 </div>
-            </footer>
-        </div>
+            </div>
+        </footer>
+    </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    
+
     <script src="js/scripts.js"></script>
-    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -229,77 +229,33 @@ $sensor_data = $stmt->get_result();
 
 
 
-<script>
-var xValues = [];
-
-var yValues = [];
-
-<?php foreach($sensor_data as $row){
-
-$temp =  (float)$row['Temperature'];
-
-$humidity = (float)$row['Humidity'];
-
-$time = $row['Record_Time'];
-
-?>
-
-xValues.push('<?php echo $time; ?>');
-
-yValues.push(<?php echo $humidity; ?>);
-
-
-<?php }?>
-
-console.log(xValues);
-
-console.log(yValues);
-
-new Chart("areaChart", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{
-      fill: false,
-      lineTension: 0,
-      backgroundColor: "rgba(0,0,255,1.0)",
-      borderColor: "rgba(0,0,255,0.1)",
-      data: yValues
-    }]
-  },
-  options: {
-    legend: {display: false},
-    scales: {
-      yAxes: [{ticks: {min: 0, max:100}}],
-    }
-  }
-});
-</script>
-
-
-
-
-
     <script>
-        const xValues = [];
+        var xValues = [];
 
-        const yValues = [];
+        var yValues = [];
 
         <?php foreach ($sensor_data as $row) {
 
+            $temp =  (float)$row['Temperature'];
+
             $humidity = (float)$row['Humidity'];
 
-            $temperature = (float)['Temperature'];
+            $time = $row['Record_Time'];
 
         ?>
 
+            xValues.push('<?php echo $time; ?>');
+
             yValues.push(<?php echo $humidity; ?>);
 
-            xValues.push(<?php echo $temperature; ?>);
 
         <?php } ?>
 
-        new Chart("line_graps", {
+        console.log(xValues);
+
+        console.log(yValues);
+
+        new Chart("areaChart", {
             type: "line",
             data: {
                 labels: xValues,
@@ -318,53 +274,15 @@ new Chart("areaChart", {
                 scales: {
                     yAxes: [{
                         ticks: {
-                            min: 6,
-                            max: 16
+                            min: 0,
+                            max: 100
                         }
                     }],
                 }
             }
         });
     </script>
-
-    <script>
-        google.charts.load('current', {
-            packages: ['corechart']
-        });
-        google.charts.setOnLoadCallback(drawChart);
-
-        function drawChart() {
-
-            const data = google.visualization.arrayToDataTable([
-                ['Price', 'Size'],
-
-                <?php foreach ($sensor_data as $row) {
-                    $temp =  (float)$row['Temperature'];
-
-                    $humidity = (float)$row['Humidity'];
-                ?>
-
-                    [<?php echo $temp ?>, <?php echo $humidity ?>],
-
-                <?php } ?>
-            ]);
-
-            const options = {
-                title: 'House Prices vs. Size',
-                hAxis: {
-                    title: 'Square Meters'
-                },
-                vAxis: {
-                    title: 'Price in Millions'
-                },
-                legend: 'none'
-            };
-            // Draw
-            const chart = new google.visualization.LineChart(document.getElementById('myChart3'));
-            chart.draw(data, options);
-        }
-    </script>
-
+    
 </body>
 
 </html>
