8602a44f34d7f82b88a8a6845a63e9ea864078fa
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-04-24 19:58:16 +0530

    create head.php

---
+++ assets/components/head.php
@@ -0,0 +1,11 @@
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>IceBurg Dashboard</title>
+    <link href="css/styles.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+</head>
