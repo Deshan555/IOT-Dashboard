ce8d6ea4c12ef34531508ac0c898e3c69958b6f9
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-04-24 21:52:44 +0530

    link footer.php file

--- humidity.php
+++ humidity.php
@@ -16,46 +16,12 @@ $sensor_data = $stmt->get_result();
 
 <html lang="en">
 
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Charts - SB Admin</title>
-    <link href="css/styles.css" rel="stylesheet" />
-    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-</head>
+<?php include 'assets/components/head.php' ?>
 
 <body class="sb-nav-fixed">
-    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-        <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
-        <!-- Sidebar Toggle-->
-        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-        <!-- Navbar Search-->
-        <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-            <div class="input-group">
-                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
-            </div>
-        </form>
-        <!-- Navbar-->
-        <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#!">Settings</a></li>
-                    <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                    <li>
-                        <hr class="dropdown-divider" />
-                    </li>
-                    <li><a class="dropdown-item" href="#!">Logout</a></li>
-                </ul>
-            </li>
-        </ul>
-    </nav>
+
+    <?php include 'assets/components/navbar.php' ?>
+
     <?php include 'sidebar.php' ?>
 
     <div id="layoutSidenav_content">
@@ -68,9 +34,7 @@ $sensor_data = $stmt->get_result();
                 </ol>
                 <div class="card mb-4">
                     <div class="card-body">
-                        Chart.js is a third party plugin that is used to generate the charts in this template. The charts below have been customized - for further customization options, please visit the official
-                        <a target="_blank" href="https://www.chartjs.org/docs/latest/">Chart.js documentation</a>
-                        .
+                    This dashboard uses line charts, pie charts, and bar charts to display humidity statistics for the given time period. Using datatable, you may view more information
                     </div>
                 </div>
                 <div class="card mb-4">
@@ -158,20 +122,10 @@ $sensor_data = $stmt->get_result();
                                 </table>
                             </div>
                         </div>
-            </div>
-        </main>
-        <footer class="py-4 bg-light mt-auto">
-            <div class="container-fluid px-4">
-                <div class="d-flex align-items-center justify-content-between small">
-                    <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                    <div>
-                        <a href="#">Privacy Policy</a>
-                        &middot;
-                        <a href="#">Terms &amp; Conditions</a>
-                    </div>
                 </div>
-            </div>
-        </footer>
+        </main>
+
+        <?php include 'assets/components/footer.php'?>
     </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
@@ -222,7 +176,7 @@ $sensor_data = $stmt->get_result();
             options: {
                 title: {
                     display: true,
-                    text: "World Wide Wine Production 2018"
+                    text: "Latest Humidity(%) Comparison Chart"
                 }
             }
         });
@@ -267,11 +221,28 @@ $sensor_data = $stmt->get_result();
                 },
                 title: {
                     display: true,
-                    text: "World Wine Production 2018"
+                    text: "Latest Humidity(%) Comparison Chart"
+                },
+                scales: {
+                    yAxes: [{
+                        scaleLabel:{
+                            display: true,
+                            labelString: 'Humidity(%)'
+                        }}],
+                    
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Capture Time'
+                        }}],
                 }
             }
         });
     </script>
+
+
+    <!-- SCRIPT FOR LINE-CHART-->
 
     <script>
         var xValues = [];
@@ -318,14 +289,25 @@ $sensor_data = $stmt->get_result();
                         ticks: {
                             min: 0,
                             max: 100
-                        }
-                    }],
-                }
-            }
+                        },
+                        scaleLabel:{
+                            display: true,
+                            labelString: 'Humidity(%)'
+                        }}],
+                    
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Capture Time'
+                        }}],
+                }}
         });
     </script>
     
 </body>
+
+
 
 <!-- ADD JS SCRIPTS FOR DATATABLES HERE-->
 
