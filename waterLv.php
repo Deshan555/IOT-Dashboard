e91b9f9b5589e28a1b3258c9d03375e398b24668
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-04-25 11:31:16 +0530

    complete waterlv.php backend and remove unwanted codelines and comments from .php file

--- waterLv.php
+++ waterLv.php
@@ -2,7 +2,7 @@
 
 include('php/config.php');
 
-$SQL = "SELECT * FROM `sensor_box_01` LIMIT 5;";
+$SQL = "SELECT * FROM `sensor_box_02` LIMIT 5;";
 
 $stmt = $conn->prepare($SQL);
 
@@ -16,66 +16,31 @@ $sensor_data = $stmt->get_result();
 
 <html lang="en">
 
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Charts - SB Admin</title>
-    <link href="css/styles.css" rel="stylesheet" />
-    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-</head>
+<?php include 'assets/components/head.php' ?>
 
 <body class="sb-nav-fixed">
-    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-        <!-- Navbar Brand-->
-        <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
-        <!-- Sidebar Toggle-->
-        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-        <!-- Navbar Search-->
-        <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-            <div class="input-group">
-                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-                <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
-            </div>
-        </form>
-        <!-- Navbar-->
-        <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#!">Settings</a></li>
-                    <li><a class="dropdown-item" href="#!">Activity Log</a></li>
-                    <li>
-                        <hr class="dropdown-divider" />
-                    </li>
-                    <li><a class="dropdown-item" href="#!">Logout</a></li>
-                </ul>
-            </li>
-        </ul>
-    </nav>
+
+    <?php include 'assets/components/navbar.php' ?>
+
     <?php include 'sidebar.php' ?>
 
     <div id="layoutSidenav_content">
         <main>
             <div class="container-fluid px-4">
-                <h1 class="mt-4">HUMIDITY</h1>
+                <h1 class="mt-4">Water Level</h1>
                 <ol class="breadcrumb mb-4">
-                    <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                    <li class="breadcrumb-item active">Humidity</li>
+                    <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
+                    <li class="breadcrumb-item active">WaterLevel</li>
                 </ol>
                 <div class="card mb-4">
                     <div class="card-body">
-                        Chart.js is a third party plugin that is used to generate the charts in this template. The charts below have been customized - for further customization options, please visit the official
-                        <a target="_blank" href="https://www.chartjs.org/docs/latest/">Chart.js documentation</a>
-                        .
+                        This dashboard uses line charts, pie charts, and bar charts to display Water Level statistics for the given time period. Using datatable, you may view more information
                     </div>
                 </div>
                 <div class="card mb-4">
                     <div class="card-header">
                         <i class="fas fa-chart-area me-1"></i>
-                        Area Chart Example
+                        Area Chart
                     </div>
                     <!--<div class="card-body"><canvas id="myAreaChart" width="100%" height="30"></canvas></div>-->
                     <!--<div class="card-body"><canvas id="line_graps" style="width:100%;max-width:600px"></canvas></div>-->
@@ -87,7 +52,7 @@ $sensor_data = $stmt->get_result();
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-chart-bar me-1"></i>
-                                Bar Chart Example
+                                Bar Chart
                             </div>
                             <!--<div class="card-body"><canvas id="myBarChart" width="100%" height="50"></canvas></div>-->
                             <!--<div id="piechart" style="width:100%; max-width:600px; height:500px;"></div>-->
@@ -99,7 +64,7 @@ $sensor_data = $stmt->get_result();
                         <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-chart-pie me-1"></i>
-                                Pie Chart Example
+                                Pie Chart
                             </div>
                             <!--<div class="card-body"><canvas id="myPieChart" width="100%" height="50"></canvas></div>-->
                             <!--<div id="myChart" style="width:100%; max-width: 900px; height:500px;"></div>-->
@@ -108,20 +73,59 @@ $sensor_data = $stmt->get_result();
                         </div>
                     </div>
                 </div>
-            </div>
-        </main>
-        <footer class="py-4 bg-light mt-auto">
-            <div class="container-fluid px-4">
-                <div class="d-flex align-items-center justify-content-between small">
-                    <div class="text-muted">Copyright &copy; Your Website 2023</div>
-                    <div>
-                        <a href="#">Privacy Policy</a>
-                        &middot;
-                        <a href="#">Terms &amp; Conditions</a>
+
+                <?php
+
+                $SQL = "SELECT * FROM `sensor_box_02`;";
+
+                $stmt = $conn->prepare($SQL);
+
+                $stmt->execute();
+
+                $all_data = $stmt->get_result();
+
+                ?>
+
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <i class="fas fa-table me-1"></i>
+                        DataTable
+                    </div>
+                    <div class="card-body">
+                        <table id="datatablesSimple">
+                            <thead>
+                                <tr>
+                                    <th>Record ID</th>
+                                    <th>Record Date</th>
+                                    <th>Record Time</th>
+                                    <th>Water Level (lv/100)</th>
+                                </tr>
+                            </thead>
+                            <tfoot>
+                                <tr>
+                                    <th>Record ID</th>
+                                    <th>Record Date</th>
+                                    <th>Record Time</th>
+                                    <th>Water Level (lv/100)</th>
+                                </tr>
+                            </tfoot>
+                            <tbody>
+                                <?php foreach ($all_data as $row) { ?>
+                                    <tr>
+                                        <td><?php echo $row['Record_ID'] ?></td>
+                                        <td><?php echo $row['Record_Date'] ?></td>
+                                        <td><?php echo $row['Record_Time'] ?></td>
+                                        <td><?php echo $row['Water_Level'] ?></td>
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
-        </footer>
+        </main>
+
+        <?php include 'assets/components/footer.php' ?>
     </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
@@ -141,7 +145,7 @@ $sensor_data = $stmt->get_result();
 
         <?php foreach ($sensor_data as $row) {
 
-            $humidity = (float)$row['Humidity'];
+            $humidity = (float)$row['Water_Level'];
 
             $record_time = $row['Record_Time'];
         ?>
@@ -172,7 +176,7 @@ $sensor_data = $stmt->get_result();
             options: {
                 title: {
                     display: true,
-                    text: "World Wide Wine Production 2018"
+                    text: "Latest Water_Level Comparison Chart"
                 }
             }
         });
@@ -188,7 +192,7 @@ $sensor_data = $stmt->get_result();
 
         <?php foreach ($sensor_data as $row) {
 
-            $humidity = (float)$row['Humidity'];
+            $humidity = (float)$row['Water_Level'];
 
             $time = $row['Record_Time'];
 
@@ -217,11 +221,30 @@ $sensor_data = $stmt->get_result();
                 },
                 title: {
                     display: true,
-                    text: "World Wine Production 2018"
+                    text: "Latest Water_Level Comparison Chart"
+                },
+                scales: {
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Water_Level'
+                        }
+                    }],
+
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Capture Time'
+                        }
+                    }],
                 }
             }
         });
     </script>
+
+
+    <!-- SCRIPT FOR LINE-CHART-->
 
     <script>
         var xValues = [];
@@ -230,7 +253,7 @@ $sensor_data = $stmt->get_result();
 
         <?php foreach ($sensor_data as $row) {
 
-            $humidity = (float)$row['Humidity'];
+            $humidity = (float)$row['Water_Level'];
 
             $time = $row['Record_Time'];
 
@@ -268,13 +291,33 @@ $sensor_data = $stmt->get_result();
                         ticks: {
                             min: 0,
                             max: 100
+                        },
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Water_Level'
+                        }
+                    }],
+
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Capture Time'
                         }
                     }],
                 }
             }
         });
     </script>
-    
+
 </body>
+
+
+
+<!-- ADD JS SCRIPTS FOR DATATABLES HERE-->
+
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+
+<script src="js/datatables-simple-demo.js"></script>
 
 </html>
